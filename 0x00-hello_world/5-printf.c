1d89ba5c1522c08bc2c37f757f8d626a1e50560d
Sagaxman <gbengadadebayo@gmail.com>
2023-09-07 15:22:00 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is a special function that serves as the entry point for the program
+ *
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

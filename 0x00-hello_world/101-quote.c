fd57fdeb03770b2d32306b648a5bf91bc2ebeafe
Sagaxman <gbengadadebayo@gmail.com>
2023-09-07 18:01:01 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,11 @@
 /**
  * main - a c program that prints a line to the standard error
  *
- * Return: 1
+ * Return: always (1)
  */
 
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2 "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }

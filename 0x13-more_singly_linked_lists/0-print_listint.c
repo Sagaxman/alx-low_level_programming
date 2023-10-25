5e423c7e11b8f3858aa217fd29d83c09b3d5a239
Sagaxman <gbengadadebayo@gmail.com>
2023-10-24 19:42:27 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,12 +8,17 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t cont = 0;
+	size_t i = 0;
+	const listint_t *current;
 
-	for (; h != NULL; cont++)
+	current = h;
+
+	while (current != NULL)
 	{
-		printf("%d\n", h->n);
-		h = h->next;
+		printf("%d\n", current->n);
+		current = current->next;
+		i++;
 	}
-	return (cont);
+
+	return (i);
 }

af56f6f34d8c3ed7ee1f60ca8a3135bfd8a439f5
Sagaxman <gbengadadebayo@gmail.com>
2023-10-24 18:35:57 -0700

    100-reverse_listint.c

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+#include <stdlib.h> // Include the necessary header for malloc
+
+/**
+ * insert_nodeint_at_index - inserts a new node in a linked list,
+ * at a given position
+ * @head: pointer to the first node in the list
+ * @idx: index where the new node is added
+ * @n: data to insert in the new node
+ *
+ * Return: pointer to the new node, or NULL
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+    unsigned int i;
+    listint_t *new;
+    listint_t *temp = *head;
+
+    new = malloc(sizeof(listint_t)); // Check if malloc succeeded
+    if (!new)
+        return (NULL);
+
+    new->n = n;
+    new->next = NULL;
+
+    if (idx == 0)
+    {
+        new->next = *head;
+        *head = new;
+        return (new);
+    }
+
+    for (i = 0; temp && i < idx - 1; i++)
+    {
+        temp = temp->next;
+        if (!temp) // Check if temp is NULL before accessing temp->next
+        {
+            free(new); // Free the newly allocated node
+            return (NULL);
+        }
+    }
+
+    new->next = temp->next;
+    temp->next = new;
+    return (new);
+}
